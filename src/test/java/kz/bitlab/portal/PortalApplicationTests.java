37fedeebc775e4faa8d8086513b6c5f4328a518c
shyngysbekulym <78029910+shyngysbekulym@users.noreply.github.com>
2024-10-18 16:49:29 +0500

    test commet

---
+++ src/test/java/kz/bitlab/portal/PortalApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
